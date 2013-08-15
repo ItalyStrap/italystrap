2c5a4d4df54a685cd0b52bd0991dae8d2ea0086f
Enea Overclokk <info@overclokk.net>
2013-08-15 20:55:39 +0200

    Revert "Start"
    This reverts commit e1224c96d304b43c5ef2d8f93f708e21a787141d.

--- sidebar.php
+++
@@ -1,3 +0,0 @@
-<aside class="span4" itemscope itemtype="http://schema.org/WPSideBar">
-	<?php dynamic_sidebar('Sidebar'); ?>
-</aside>
--- template/non-trovato.php
+++
@@ -1,7 +0,0 @@
-<article id="post-0" class="post no-results not-found">
-	<header class="page-header">
-		<h1 itemprop="headline"><?php _e( 'Azz, quello che stavi cercando non c\'&egrave;', 'Italystrap' ); ?></h1>
-	</header>
-		<p><?php _e( 'Controlla di aver scritto correttamente la parola o frase oppure cerca ancora usando il form qui sotto.', 'twentytwelve' ); ?></p>
-		<?php get_search_form(); ?>
-</article><!-- #post-0 -->

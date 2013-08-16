6f3ca40b9758998c42a17592e0ac7f338d211fb6
Enea Overclokk <info@overclokk.net>
2013-08-16 10:59:47 +0200

    Spostato custom post type su altro file

--- functions.php
+++ functions.php
@@ -1,8 +1,10 @@
 <?php
-require_once locate_template('/lib/cleanup.php');        	// Cleanup
-require_once locate_template('/lib/breadcrumb.php');      	// Breadcrumb
+require_once locate_template('/lib/cleanup.php');        		//Cleanup
+require_once locate_template('/lib/breadcrumb.php');      		//Breadcrumb
 require_once locate_template('/lib/widget.php');
-require_once locate_template('/lib/wp_bootstrap_gallery.php');					// Register Custom Gallery: https://github.com/twittem/wp-bootstrap-gallery
+require_once locate_template('/lib/wp_bootstrap_gallery.php');	//Register Custom Gallery:https://github.com/twittem/wp-bootstrap-gallery
+require_once locate_template('/lib/custom-post-type.php');
+
 /*
  * Setup Theme Functions
  */
@@ -175,51 +177,6 @@ add_image_size( 'article-thumb', 760, 380, true);
 add_image_size( 'article-thumb-index', 260, 130, true);
 add_image_size( 'full-width', 1170, 585, true);
 
-// Register Custom Post Type
-function custom_post_type() {
-	$labels = array(
-		'name'                => _x( 'Prodotti', 'Post Type General Name', 'ItalyStrap' ),
-		'singular_name'       => _x( 'Prodotti', 'Post Type Singular Name', 'ItalyStrap' ),
-		'menu_name'           => __( 'Prodotti', 'ItalyStrap' ),
-		'parent_item_colon'   => __( 'Parent prodotti:', 'ItalyStrap' ),
-		'all_items'           => __( 'Tutti i prodotti', 'ItalyStrap' ),
-		'view_item'           => __( 'Visualizza i prodotti', 'ItalyStrap' ),
-		'add_new_item'        => __( 'Aggiungi nuovi prodotti', 'ItalyStrap' ),
-		'add_new'             => __( 'Aggiungi nuovi prodotti', 'ItalyStrap' ),
-		'edit_item'           => __( 'Modifica prodotti', 'ItalyStrap' ),
-		'update_item'         => __( 'Aggiorna prodotti', 'ItalyStrap' ),
-		'search_items'        => __( 'Ricerca prodotti', 'ItalyStrap' ),
-		'not_found'           => __( 'Nessun prodotto trovato', 'ItalyStrap' ),
-		'not_found_in_trash'  => __( 'Nessun prodotto nel cestino', 'ItalyStrap' ),
-	);
-
-	$args = array(
-		'label'               => __( 'prodotti', 'ItalyStrap' ),
-		'description'         => __( 'prodotti information pages', 'ItalyStrap' ),
-		'labels'              => $labels,
-		'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'custom-fields', ),
-		'taxonomies'          => array( 'category', 'post_tag', 'Slide' ),
-		'hierarchical'        => true,
-		'public'              => true,
-		'show_ui'             => true,
-		'show_in_menu'        => true,
-		'show_in_nav_menus'   => true,
-		'show_in_admin_bar'   => true,
-		'menu_position'       => 5,
-		'menu_icon'           => '',
-		'can_export'          => true,
-		'has_archive'         => true,
-		'exclude_from_search' => false,
-		'publicly_queryable'  => true,
-		'rewrite'             => true,
-		'capability_type'     => 'page',
-	);
-
-	register_post_type( 'prodotti', $args );
-}
-
-// Hook into the 'init' action
-add_action( 'init', 'custom_post_type', 0 );
 
 
 add_action('wp_insert_post', 'italystrap_set_default_custom_fields');

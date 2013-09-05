e8d2a8348d30f5bee4eb5725e95e0cf3b909caa6
Enea Overclokk <info@overclokk.net>
2013-09-05 09:46:31 +0200

    Separate script from functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 require_once locate_template('/lib/cleanup.php');        		//Cleanup
+require_once locate_template('/lib/script.php');        		//Cleanup
 require_once locate_template('/lib/breadcrumb.php');      		//Breadcrumb
 require_once locate_template('/lib/widget.php');
 require_once locate_template('/lib/wp_bootstrap_gallery.php');	//Register Custom Gallery:https://github.com/twittem/wp-bootstrap-gallery
@@ -55,50 +56,6 @@ function ri_wp_favicon()
 }
 add_action('wp_head', 'ri_wp_favicon');
 
-//Carico gli stili CSS
-function italystrap_add_style()
-	{
-		global $path;
-		wp_enqueue_style( 'bootstrap',  $path . '/css/bootstrap.min.css', null, null);
-		wp_enqueue_style( 'style',  $path . '/style.css', null, null);
-	}
-if (!is_admin()) 
-	{
-		add_action( 'wp_enqueue_scripts', 'italystrap_add_style' ); 
-	}
-
-//Carico jquery da google api cdn
-function italystrap_add_jquery_googlecdn() 
-	{
-		wp_deregister_script('jquery');
-		wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, null, true);
-		wp_enqueue_script('jquery');
-	}
-if (!is_admin())
-	{
-		add_action('init', 'italystrap_add_jquery_googlecdn');
-	}
-
-//Carico gli script JS
-function italystrap_add_javascripts() 
-	{
-		global $path;
-		wp_enqueue_script( 'bootstrap', $path . '/js/bootstrap.min.js', null, null,  true );
-		//wp_enqueue_script('socialite', $path . '/js/socialite.js', null, null, true);
-	}
-if (!is_admin()) 
-	{
-		add_action( 'wp_print_scripts', 'italystrap_add_javascripts' ); 
-	}
-	
-	
-//http://www.emoticode.net/php/add-async-and-defer-to-script-on-wordpress.html
-// function defer_parsing_of_js ( $url ) {
-	// if ( FALSE === strpos( $url, '.js' ) ) return $url;
-	// if ( strpos( $url, 'jquery.js' ) ) return $url;
-	// return "$url' async defer";
-// }
-	//add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
 
 //Registro l'area widget classica nella sidebar
 if (function_exists('register_sidebar'))
---
+++ lib/script.php
@@ -0,0 +1,46 @@
+<?php
+//Carico gli stili CSS
+function italystrap_add_style()
+	{
+		global $path;
+		wp_enqueue_style( 'bootstrap',  $path . '/css/bootstrap.min.css', null, null);
+		wp_enqueue_style( 'style',  $path . '/style.css', null, null);
+	}
+if (!is_admin()) 
+	{
+		add_action( 'wp_enqueue_scripts', 'italystrap_add_style' ); 
+	}
+
+//Carico jquery da google api cdn
+function italystrap_add_jquery_googlecdn() 
+	{
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, null, true);
+		wp_enqueue_script('jquery');
+	}
+if (!is_admin())
+	{
+		add_action('init', 'italystrap_add_jquery_googlecdn');
+	}
+
+//Carico gli script JS
+function italystrap_add_javascripts() 
+	{
+		global $path;
+		wp_enqueue_script( 'bootstrap', $path . '/js/bootstrap.min.js', null, null,  true );
+		//wp_enqueue_script('socialite', $path . '/js/socialite.js', null, null, true);
+	}
+if (!is_admin()) 
+	{
+		add_action( 'wp_print_scripts', 'italystrap_add_javascripts' ); 
+	}
+	
+	
+//http://www.emoticode.net/php/add-async-and-defer-to-script-on-wordpress.html
+// function defer_parsing_of_js ( $url ) {
+	// if ( FALSE === strpos( $url, '.js' ) ) return $url;
+	// if ( strpos( $url, 'jquery.js' ) ) return $url;
+	// return "$url' async defer";
+// }
+	//add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
+?>

61132355606e83275e954a39f1b2bbdbe0f2f067
Enea Overclokk <info@overclokk.net>
2014-05-15 21:21:18 +0200

    Fix some bug in breadcrumbs

--- lib/breadcrumb.php
+++ lib/breadcrumb.php
@@ -1,101 +1,52 @@
 <?php
-function create_breadcrumbs() {
-  
-  $delimiter = '/';
-  $home = 'Home';
-  $before = '<li class="active">';
-  $after = '</li>';
-  
-  if ( !is_home() && !is_front_page() || is_paged() ) {
-  
-    echo '<div itemscope itemtype="http://schema.org/WebPage"><ul class="breadcrumb" itemprop="breadcrumb">';
-  
-    global $post;
-    $homeLink = get_bloginfo('url');
-    echo '<li><a href="' . $homeLink . '">' . $home . '</a></li> ' . $delimiter . ' ';
-  
-    if ( is_category() ) {
-      global $wp_query;
-      $cat_obj = $wp_query->get_queried_object();
-      $thisCat = $cat_obj->term_id;
-      $thisCat = get_category($thisCat);
-      $parentCat = get_category($thisCat->parent);
-      if ($thisCat->parent != 0) echo(get_category_parents($parentCat, TRUE, ' ' . $delimiter . ' '));
-      echo $before . single_cat_title('', false) . $after;
-  
-    } elseif ( is_day() ) {
-      echo '<li><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a></li> ' . $delimiter . ' ';
-      echo '<li><a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('F') . '</a></li> ' . $delimiter . ' ';
-      echo $before . get_the_time('d') . $after;
-  
-    } elseif ( is_month() ) {
-      echo '</li><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a></li> ' . $delimiter . ' ';
-      echo $before . get_the_time('F') . $after;
-  
-    } elseif ( is_year() ) {
-      echo $before . get_the_time('Y') . $after;
-  
-    } elseif ( is_single() && !is_attachment() ) {
-      if ( get_post_type() != 'post' ) {
-        $post_type = get_post_type_object(get_post_type());
-        $slug = $post_type->rewrite;
-        echo '<li><a href="' . $homeLink . '/' . $slug['slug'] . '/">' . $post_type->labels->singular_name . '</a></li> ' . $delimiter . ' ';
-        echo $before . get_the_title() . $after;
-      } else {
-        $cat = get_the_category(); $cat = $cat[0];
-        echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
-        echo $before . get_the_title() . $after;
+function create_breadcrumbs() { //http://mkoerner.de/breadcrumbs-for-wordpress-themes-with-bootstrap-3/
+  if(!is_home()) {
+    echo '<div itemscope itemtype="http://schema.org/WebPage"><ol class="breadcrumb" itemprop="breadcrumb">';
+    echo '<li><a href="'.get_option('home').'">Home</a></li>';
+    if (is_single()) {
+      echo '<li>';
+      the_category(', ');
+      echo '</li>';
+      if (is_single()) {
+        echo '<li>';
+        the_title();
+        echo '</li>';
       }
-  
-    } elseif ( !is_single() && !is_page() && get_post_type() != 'post' && !is_404() ) {
-      $post_type = get_post_type_object(get_post_type());
-      echo $before . $post_type->labels->singular_name . $after;
-  
-    } elseif ( is_attachment() ) {
-      $parent = get_post($post->post_parent);
-      $cat = get_the_category($parent->ID); $cat = $cat[0];
-      echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
-      echo '</li><a href="' . get_permalink($parent) . '">' . $parent->post_title . '</a></li> ' . $delimiter . ' ';
-      echo $before . get_the_title() . $after;
-  
-    } elseif ( is_page() && !$post->post_parent ) {
-      echo $before . get_the_title() . $after;
-  
-    } elseif ( is_page() && $post->post_parent ) {
-      $parent_id  = $post->post_parent;
-      $breadcrumbs = array();
-      while ($parent_id) {
-        $page = get_page($parent_id);
-        $breadcrumbs[] = '</li><a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a></li>';
-        $parent_id  = $page->post_parent;
-      }
-      $breadcrumbs = array_reverse($breadcrumbs);
-      foreach ($breadcrumbs as $crumb) echo $crumb . ' ' . $delimiter . ' ';
-      echo $before . get_the_title() . $after;
-  
-    } elseif ( is_search() ) {
-      echo $before . get_search_query() . $after;
-  
-    } elseif ( is_tag() ) {
-      echo $before . single_tag_title('', false) . $after;
-  
-    } elseif ( is_author() ) {
-       global $author;
-      $userdata = get_userdata($author);
-      echo $before . $userdata->display_name . $after;
-  
-    } elseif ( is_404() ) {
-      echo $before . 'Error 404' . $after;
+    } elseif (is_category()) {
+      echo '<li>';
+      single_cat_title();
+      echo '</li>';
+    } elseif (is_page() && (!is_front_page())) {
+      echo '<li>';
+      the_title();
+      echo '</li>';
+    } elseif (is_tag()) {
+      echo '<li>Tag: ';
+      single_tag_title();
+      echo '</li>';
+    } elseif (is_day()) {
+      echo'<li>Archive for ';
+      the_time('F jS, Y');
+      echo'</li>';
+    } elseif (is_month()) {
+      echo'<li>Archive for ';
+      the_time('F, Y');
+      echo'</li>';
+    } elseif (is_year()) {
+      echo'<li>Archive for ';
+      the_time('Y');
+      echo'</li>';
+    } elseif (is_author()) {
+      echo'<li>Author Archives';
+      echo'</li>';
+    } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {
+      echo '<li>Blog Archives';
+      echo'</li>';
+    } elseif (is_search()) {
+      echo'<li>Search Results';
+      echo'</li>';
     }
-  
-    if ( get_query_var('paged') ) {
-      if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ' (';
-      echo __('Page') . ' ' . get_query_var('paged');
-      if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ')';
-    }
-  
-    echo '</ul></div>';
-  
+    echo '</ol></div>';
   }
 }
 ?>

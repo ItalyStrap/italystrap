3266290ffc74035e37cfac74bd164af43fef580b
Enea Overclokk <info@overclokk.net>
2014-09-02 21:07:41 +0200

    Fix echo current page in CPT for Facebook OG

--- lib/facebook_opengraph.php
+++ lib/facebook_opengraph.php
@@ -9,7 +9,13 @@
     <meta property="og:description" content="<?php bloginfo('description'); ?>" />
     <meta property="og:type" content="website" />
     <meta property="og:image" content="<?php echo italystrap_logo();?>" /><!-- Min image size 200x200px -->
-	<meta property="og:url" content="<?php echo site_url(); ?>" />
+    <?php 
+    /**
+     * Get the current page (For custom post category)
+     * @link https://core.trac.wordpress.org/ticket/9963
+     */
+    ?>
+	<meta property="og:url" content="<?php echo $current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>" />
 	<?php } ?>	
 	<?php if ( is_singular() ) {?>
 <!-- Facebook Opengraph Single or Page -->

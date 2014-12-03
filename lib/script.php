c9f9a6ac573395f7237ee85e35ec345a4300a778
Enea Overclokk <info@overclokk.net>
2014-12-03 12:37:32 +0100

    Improve load JS and CSS on hierarchy of page

--- lib/script.php
+++ lib/script.php
@@ -1,39 +1,75 @@
 <?php
+/**
+ * Use this file for loading any scripts and styles
+ * If you use child theme copy it in child folder and change $path if necessary
+ */
 function italystrap_add_style_and_script(){
 
+		/**
+		 * @variable $path is path for parent template
+		 * @variable $pathchild is for child template
+		 */
 		global $path;
 		global $pathchild;
 
-		//Carico gli stili CSS
-		//wp_enqueue_style( 'style',  $path . '/style.css', null, null, null);
+		/**
+		 * Load Bootstrap styles
+		 */
 		wp_enqueue_style( 'bootstrap',  $path . '/css/bootstrap.min.css', null, null, null);
-		wp_enqueue_style( 'italystrap-style',  $path . '/css/style.css', array('bootstrap'), null, null);
-		//wp_enqueue_style( 'custom',  $path . '/css/custom.css', array('bootstrap'), null, null);
 
-		//Carico jquery da google api cdn
+		/**
+		 * Deregister jquery from WP
+		 */
 		wp_deregister_script('jquery');
+
+		/**
+		 * Load jquery from google CDN
+		 */
 		wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', false, null, true);
+		
+		/**
+		 * If CDN is down load from callback
+		 */
 		add_filter('script_loader_src', 'italystrap_jquery_local_fallback', 10, 2);
 		wp_enqueue_script('jquery');
 
-		//Carico gli script JS
-		wp_enqueue_script( 'bootstrap', $path . '/js/bootstrap.min.js', array('jquery'), null,  true );
-		//wp_enqueue_script('socialite', $path . '/js/socialite.js', null, null, true);
-		//Activate slide on windows load
+		/**
+		 * Load script JS and CSS with conditional tags
+		 */
 		if ( is_home() || is_front_page() ){
+
 			wp_enqueue_style( 'home',  $path . '/css/home.css', array('bootstrap'), null, null);
-			wp_enqueue_script( 'home', $path . '/js/home.min.js', array('jquery' , 'bootstrap'), null,  true );
-		}
-		if ( !is_home() || !is_front_page() ) {
-			wp_enqueue_script( 'custom', $path . '/js/custom.min.js', array('jquery' , 'bootstrap'), null,  true );
+			wp_enqueue_script( 'home', $path . '/js/home.min.js', array('jquery'), null,  true );
+
+		}elseif ( is_singular() ) {
+
+			wp_enqueue_style( 'singular',  $path . '/css/singular.css', array('bootstrap'), null, null);
+			wp_enqueue_script( 'singular', $path . '/js/singular.min.js', array('jquery'), null,  true );
+
+		}elseif ( is_archive() ) {
+
+			wp_enqueue_style( 'archive',  $path . '/css/archive.css', array('bootstrap'), null, null);
+			wp_enqueue_script( 'archive', $path . '/js/archive.min.js', array('jquery'), null,  true );
+
+		}else {
+
+			wp_enqueue_style( 'custom',  $path . '/css/custom.css', array('bootstrap'), null, null);
+			wp_enqueue_script( 'custom', $path . '/js/custom.min.js', array('jquery'), null,  true );
+
 		}
 
+		/**
+		 * Load comment-reply script
+		 */
 		if (is_singular() && comments_open() && get_option('thread_comments')) {
     		wp_enqueue_script('comment-reply');
   		}
 	}
-// http://wordpress.stackexchange.com/a/12450
-// https://github.com/roots/roots/blob/master/lib/scripts.php
+
+/**
+ * http://wordpress.stackexchange.com/a/12450
+ * https://github.com/roots/roots/blob/master/lib/scripts.php
+ */
 function italystrap_jquery_local_fallback($src, $handle = null){
 
 		global $path;
@@ -51,8 +87,11 @@ function italystrap_jquery_local_fallback($src, $handle = null){
 		return $src;
 	}
 
-if (!is_admin()) 
-	{
+/**
+ * If is not admin load scripts and styles
+ */
+if ( !is_admin() ){
+
 		add_action( 'wp_enqueue_scripts', 'italystrap_add_style_and_script' ); 
 		add_action( 'wp_footer', 'italystrap_jquery_local_fallback' );
 	}

e7e95a2f1ad61b1031f378fe9e558d4132b0defe
Enea Overclokk <info@overclokk.net>
2015-01-28 18:15:03 +0100

    Improve script for debug

--- lib/debug.php
+++ lib/debug.php
@@ -1,19 +1,50 @@
 <?php
 //Funzione per vedere dipendenze e script caricati nel WP-HEAD http://www.targetweb.it/eliminare-script-caricati-nel-wp-head-di-wordpress/
-//add_action('wp_head', 'debug_scripts_queued');
-//function debug_scripts_queued() {
-    //global $wp_scripts;
-    //echo '<!--- SCRIPTS TROVATI'."\r\n";
-    //foreach ( $wp_scripts->queue as $script ) {
-        //echo "\r\nSCRIPT: ".$script."\r\n";
-        //$deps = $wp_scripts->registered[$script]->deps;
-        //if ($deps) {
-            //echo "DIPENDENZE: ";
-            //print_r($deps);
-        //}
-   //}
-    //echo "\r\n--->";
-//}
 
-// var_dump()
+    // add_action('wp_head', 'debug_scripts_queued');
+    // add_action('wp_head', 'debug_styles_queued');
+
+    
+
+function debug_scripts_queued() {
+    global $wp_scripts;
+    var_dump($wp_scripts->in_footer);
+    // echo "<style>pre{display:none;}</style>";
+    echo '<pre> Script trovati in coda'."\r\n";
+    foreach ( $wp_scripts->queue as $script ) {
+        echo "\r\nScript: ".$script."\r\n";
+        $deps = $wp_scripts->registered[$script]->deps;
+        if ($deps) {
+            echo "Dipende da: ";
+            print_r($deps);
+        }else{
+            echo "Non dipende da nessuno\r\n";
+        }
+   }
+    echo "\r\n</pre>";
+}
+
+
+function debug_styles_queued() {
+    global $wp_styles;
+    var_dump($wp_styles->in_footer);
+    // echo "<style>pre{display:none;}</style>";
+    echo '<pre> Script trovati in coda'."\r\n";
+    foreach ( $wp_styles->queue as $script ) {
+        echo "\r\nScript: ".$script."\r\n";
+        $deps = $wp_styles->registered[$script]->deps;
+        if ($deps) {
+            echo "Dipende da: ";
+            print_r($deps);
+        }else{
+            echo "Non dipende da nessuno\r\n";
+        }
+   }
+    echo "\r\n</pre>";
+}
+
+
+// var_dump();
+
+// var_dump($wpdb->queries);
 ?>

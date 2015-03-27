cdc02651798588b8462fe969e2b23a86aac63966
Enea Overclokk <info@overclokk.net>
2015-03-27 22:15:40 +0100

    Add Bootstrap class to table calendar

--- js/src/archive.js
+++ js/src/archive.js
@@ -3,5 +3,7 @@ jQuery.noConflict()(function($){
 	$(document).ready(function() {
 		$('#cat').addClass('form-control');
 		$('select').addClass('form-control');
+		$('#wp-calendar').addClass('table table-hover');
+		$('td a').addClass('badge').css('margin-right', '-10px');
 	});
 });

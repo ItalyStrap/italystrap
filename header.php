054bf033788ebfcd15b47507e9ca5360cf3b905a
Enea Overclokk <info@overclokk.net>
2015-04-03 22:00:01 +0200

    Delete title because add_theme_support(title-tag')

--- header.php
+++ header.php
@@ -21,7 +21,6 @@ $lang_attr = get_bloginfo('language');
 <head>
 	<meta charset="UTF-8" >
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?php wp_title('|', true, 'right'); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		
 <?php get_template_part( 'lib/facebook_opengraph' ); ?>

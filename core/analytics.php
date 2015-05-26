fc6aa7a410ac099f25c9823b5f8d026064ec6fd0
Enea Overclokk <info@overclokk.net>
2015-05-26 20:58:12 +0200

    Added AnonymizeIP in GA

--- core/analytics.php
+++ core/analytics.php
@@ -14,7 +14,7 @@ function italystrap_add_google_analytics(){
 		?>
 
 <!-- Google Analytics from HTML5 Boilerplate  -->
-<script>(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;e=o.createElement(i);r=o.getElementsByTagName(i)[0];e.src='https://www.google-analytics.com/analytics.js';r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));ga('create','<?php echo $analytics; ?>','auto');ga('send','pageview');</script>
+<script>(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;e=o.createElement(i);r=o.getElementsByTagName(i)[0];e.src='https://www.google-analytics.com/analytics.js';r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));ga('create','<?php echo $analytics; ?>','auto');ga('send','pageview');ga('set', 'anonymizeIp', true);</script>
 		<?php
 
 		}

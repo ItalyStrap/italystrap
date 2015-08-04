8108840384a69c13fe1d0d19b89ca7222a3c5e2a
Enea Overclokk <info@overclokk.net>
2015-08-04 19:07:26 +0200

    Removed useless DOM's node

--- searchform.php
+++ searchform.php
@@ -1,21 +1,21 @@
 <?php
 /**
  * Form for search in WordPress site
+ * @link https://codex.wordpress.org/Function_Reference/get_search_form
  * Add markup for Sitelinks Search Box, @see link below for more informations
  * @link https://developers.google.com/structured-data/slsb-overview
  */
 ?>
 <div itemscope itemtype="http://schema.org/WebSite">
 	<meta itemprop="url" content="<?php echo home_url(); ?>"/>
-	<form  role="search" method="get" class="row" action="<?php echo home_url(); ?>" itemprop="potentialAction" itemscope itemtype="http://schema.org/SearchAction">
+	<form  role="search" method="get" id="searchform" class="search-form" action="<?php echo home_url(); ?>" itemprop="potentialAction" itemscope itemtype="http://schema.org/SearchAction">
 		<meta itemprop="target" content="<?php echo home_url(); ?>/?s={s}"/>
-		<div class="col-md-12">
-			<div class="input-group input-group-sm">
-					<input type="text" size="16" placeholder="<?php _e('Search now', 'ItalyStrap'); ?>" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="form-control" itemprop="query-input" >
-					<span class="input-group-btn">
-						<button type="submit" class="btn btn-default" value="<?php _e('Search', 'ItalyStrap'); ?>"><i class="glyphicon glyphicon-search"></i></button>
-					</span>
-			</div>
+		<div class="input-group input-group-sm">
+			<span class="screen-reader-text sr-only"><?php _e('Search now', 'ItalyStrap'); ?></span>
+			<input type="search" size="16" placeholder="<?php _e('Search now', 'ItalyStrap'); ?>" value="<?php if ( is_search() ) echo get_search_query();?>" name="s" class="form-control" itemprop="query-input" >
+			<span class="input-group-btn">
+				<button type="submit" class="btn btn-default" value="<?php _e('Search', 'ItalyStrap'); ?>"><i class="glyphicon glyphicon-search"></i></button>
+			</span>
 		</div>
 	</form>
 </div>

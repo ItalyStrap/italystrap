d69bb0c44ccec82345177312e91f1aad484afd21
Enea Overclokk <info@overclokk.net>
2015-08-09 16:37:19 +0200

    Added new class for widget

--- core/class-italystrap-sidebars.php
+++ core/class-italystrap-sidebars.php
@@ -5,13 +5,24 @@
 */
 class ItalyStrap_Sidebars{
 
+	private $footer_sidebars = array();
+
 	/**
 	 * Init sidebars registration
 	 */
 	function __construct(){
 
 		add_action( 'widgets_init', array( $this, 'register_sidebars' ) );
-
+		
+		$this->footer_sidebars = apply_filters(
+			'footer_sidebars_widgets',
+			array(
+			'footer-box-1',
+			'footer-box-2',
+			'footer-box-3',
+			'footer-box-4'
+			)
+		);
 	}
 
 	/**
@@ -21,64 +32,87 @@ class ItalyStrap_Sidebars{
 	public function register_sidebars(){
 
 		register_sidebar( array(
-			'name' => 'Sidebar',
-			'id' => 'sidebar-1',
-			'before_widget' => '<div id="%2$s" class="widget %2$s  col-sm-6 col-md-12">',
-			'after_widget' => "</div>",
-			'before_title' => '<h3 class="widget-title">',
-			'after_title' => '</h3>',
+			'name'				=> 'Sidebar',
+			'id'				=> 'sidebar-1',
+			'before_widget'		=> '<div id="%2$s" class="widget %2$s  col-sm-6 col-md-12">',
+			'after_widget'		=> "</div>",
+			'before_title'		=> '<h3 class="widget-title">',
+			'after_title'		=> '</h3>',
 		) );
 
 		register_sidebar( array(
-			'name' => __( 'Footer Box 1', 'ItalyStrap' ),
-			'id' => 'footer-box-1',
-			'description' => __( 'Footer box 1 widget area (Use only a widget)', 'ItalyStrap' ),
-			'before_widget' => '<div id="%2$s">',
-			'after_widget'  => '</div>',
-			'before_title' => '<h3 class="widget-title">',
-			'after_title' => '</h3>',
+			'name'				=> __( 'Footer Box 1', 'ItalyStrap' ),
+			'id'				=> 'footer-box-1',
+			'description'		=> __( 'Footer box 1 widget area (Use only a widget)', 'ItalyStrap' ),
+			'before_widget'		=> '<div id="%2$s">',
+			'after_widget' 		=> '</div>',
+			'before_title'		=> '<h3 class="widget-title">',
+			'after_title'		=> '</h3>',
 		) );
 		
 		register_sidebar( array(
-			'name' => __( 'Footer Box 2', 'ItalyStrap' ),
-			'id' => 'footer-box-2',
-			'description' => __( 'Footer box 2 widget area (Use only a widget)', 'ItalyStrap' ),
-			'before_widget' => '<div id="%2$s">',
-			'after_widget'  => '</div>',
-			'before_title' => '<h3 class="widget-title">',
-			'after_title' => '</h3>',
+			'name'				=> __( 'Footer Box 2', 'ItalyStrap' ),
+			'id'				=> 'footer-box-2',
+			'description'		=> __( 'Footer box 2 widget area (Use only a widget)', 'ItalyStrap' ),
+			'before_widget'		=> '<div id="%2$s">',
+			'after_widget' 		=> '</div>',
+			'before_title'		=> '<h3 class="widget-title">',
+			'after_title'		=> '</h3>',
 		) );
 		
 		register_sidebar( array(
-			'name' => __( 'Footer Box 3', 'ItalyStrap' ),
-			'id' => 'footer-box-3',
-			'description' => __( 'Footer box 3 widget area (Use only a widget)', 'ItalyStrap' ),
-			'before_widget' => '<div id="%2$s">',
-			'after_widget'  => '</div>',
-			'before_title' => '<h3 class="widget-title">',
-			'after_title' => '</h3>',
+			'name'				=> __( 'Footer Box 3', 'ItalyStrap' ),
+			'id'				=> 'footer-box-3',
+			'description'		=> __( 'Footer box 3 widget area (Use only a widget)', 'ItalyStrap' ),
+			'before_widget'		=> '<div id="%2$s">',
+			'after_widget' 		=> '</div>',
+			'before_title'		=> '<h3 class="widget-title">',
+			'after_title'		=> '</h3>',
 		) );
 		
 		register_sidebar( array(
-			'name' => __( 'Footer Box 4', 'ItalyStrap' ),
-			'id' => 'footer-box-4',
-			'description' => __( 'Footer box 4 widget area (Use only a widget)', 'ItalyStrap' ),
-			'before_widget' => '<div id="%2$s">',
-			'after_widget'  => '</div>',
-			'before_title' => '<h3 class="widget-title">',
-			'after_title' => '</h3>',
+			'name'				=> __( 'Footer Box 4', 'ItalyStrap' ),
+			'id'				=> 'footer-box-4',
+			'description'		=> __( 'Footer box 4 widget area (Use only a widget)', 'ItalyStrap' ),
+			'before_widget'		=> '<div id="%2$s">',
+			'after_widget' 		=> '</div>',
+			'before_title'		=> '<h3 class="widget-title">',
+			'after_title'		=> '</h3>',
 		) );
 
 		// register_sidebars(4, array(
-		// 	'name' => __( 'Footer Box %d', 'ItalyStrap' ),
-		// 	'id' => 'footer-box',
-		// 	'class' => 'footer',
-		// 	'description' => __( 'Footer box widget area (Use only a widget)', 'ItalyStrap' ),
-		// 	'before_widget' => '<div id="%2$s" class="widget %2$s col-md-3">',
-		// 	'after_widget'  => '</div>',
-		// 	'before_title' => '<h3 class="widget-title">',
-		// 	'after_title' => '</h3>',
+		// 	'name'				=> __( 'Footer Box %d', 'ItalyStrap' ),
+		// 	'id'				=> 'footer-box',
+		// 	'class'				=> 'footer',
+		// 	'description'		=> __( 'Footer box widget area (Use only a widget)', 'ItalyStrap' ),
+		// 	'before_widget'		=> '<div id="%2$s" class="widget %2$s col-md-3">',
+		// 	'after_widget' 		=> '</div>',
+		// 	'before_title'		=> '<h3 class="widget-title">',
+		// 	'after_title'		=> '</h3>',
 		// ) );
+
+		// if ( function_exists('register_sidebar') ){
+		// 	/*extract all parent pages */
+		// 	$topLevel = get_pages(array(
+		// 		'sort_column' => 'post_date',
+		// 		'hierarchical' => 0,
+		// 		'parent' => 0
+		// 		));
+
+		// 	foreach($topLevel as $page){
+		// 		/* register sidebar for each parent page */
+		// 		register_sidebar(array(  
+		// 			'name' => $page->post_title,  
+		// 			'id'   => 'sidebar-'.$page->post_name, 
+		// 			'description'   => 'This widget display on page "'.$page->post_title.'"',  
+		// 			'before_widget' => '<div id="%2$s" class="widget">',
+		// 			'after_widget'  => '</div>',  
+		// 			'before_title'  => '<h2>',  
+		// 			'after_title'   => '</h2>'  
+		// 			));
+		// 	}
+		// }
+
 
 	}
 
@@ -89,17 +123,11 @@ class ItalyStrap_Sidebars{
 	public function set_col(){
 
 		global $sidebars_widgets;
-		$this->sidebars_widgets = $sidebars_widgets;
-
-		/**
-		 * unset for retrieve only footer sidebars
-		 */
-		unset( $this->sidebars_widgets['wp_inactive_widgets'] );
-		unset( $this->sidebars_widgets['sidebar-1'] );
 
 		$count = 0;
-		foreach ($this->sidebars_widgets as $key => $value)
-			if (!empty($value))
+
+		foreach ($this->footer_sidebars as $value)
+			if ( ! empty( $sidebars_widgets[$value][0] ) )
 				$count++;
 
 		$count = ( $count === 0 ) ? 1 : $count ;
--- footer.php
+++ footer.php
@@ -7,10 +7,11 @@
  * Global variable for col-x bootstrap class in footer sidebars
  */
 global $italystrap_sidebars;
-$col = $italystrap_sidebars->set_col();
+$col = ( isset( $col ) ) ? $col : $italystrap_sidebars->set_col();
 ?>
 <!-- Footer -->
 	<footer itemscope itemtype="http://schema.org/WPFooter">
+	<?php do_action( 'footer_open' ); ?>
 			<div class="container">
 				<hr>
 				<div class="row" itemscope itemtype="http://schema.org/WPSideBar">
@@ -40,18 +41,22 @@ $col = $italystrap_sidebars->set_col();
 					<?php endif; ?>
 				</div>
 			</div>
-			<div class="container"><!-- #copyright -->
+			<div id="colophon" class="container"><!-- #copyright -->
 				<hr>
 				<div class="row">
 					<div class="col-md-12">
-						<p class="text-muted small">&copy; <span itemprop="copyrightYear"><?php echo date('Y'); ?></span> <?php bloginfo('name'); ?> | Theme developed by <a href="http://www.overclokk.net" rel="nofollow" itemprop="url">Overclokk.net</a> | Theme name: <a href="http://www.italystrap.it" rel="nofollow" itemprop="url">ItalyStrap</a> <?php if ( !is_child_theme() ): ?>| Theme version: <span class="badge" itemprop="version"><?php italystrap_version(); ?></span><?php endif; ?>
+						<p class="text-muted small">&copy; <span itemprop="copyrightYear"><?php echo date('Y'); ?></span> <?php echo esc_attr( GET_BLOGINFO_NAME ); ?> | Theme developed by <a href="http://www.overclokk.net" rel="nofollow" itemprop="url">Overclokk.net</a> | Theme name: <a href="http://www.italystrap.it" rel="nofollow" itemprop="url">ItalyStrap</a> <?php if ( !is_child_theme() ): ?>| Theme version: <span class="badge" itemprop="version"><?php italystrap_version(); ?></span><?php endif; ?>
 						</p>
 					</div>
 				</div>
 			</div><!-- #copyright -->
-
-    </footer><!-- #footer -->
+	<?php do_action( 'footer_closed' ); ?>
+	</footer><!-- #footer -->
+<?php do_action( 'wrapper_closed' ); ?>
 </div><!-- Wrapper -->
-	<?php wp_footer(); ?>
+<?php
+	wp_footer();
+	do_action( 'body_closed' );
+?>
 </body>
 </html>

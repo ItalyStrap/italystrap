208254dc5269083adaace4f510e02a0c22ad0c23
Enea Overclokk <info@overclokk.net>
2015-08-09 16:37:56 +0200

    Added standard loop for home.php

--- home.php
+++ home.php
@@ -128,6 +128,39 @@ endif;
 				wp_reset_query();
 			?>
 		</section><!-- / .row -->
+		<hr>
     </div><!-- / .container -->
 </section><!-- / #main -->
+
+	<section id="index">
+        <div class="container">
+            <div class="row">
+				<div class="col-md-8" itemscope itemtype="http://schema.org/CollectionPage">
+				<?php
+
+					if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+						get_template_part( 'loops/content', 'archive' );
+
+					endwhile;
+
+						bootstrap_pagination();
+
+					else :
+
+						get_template_part( 'loops/content', 'none');
+
+					endif;
+						wp_reset_query();
+						wp_reset_postdata();
+?>
+
+
+				</div><!-- / .col-md-8 -->
+				<?php get_sidebar(); ?> 
+			</div><!-- / .row -->
+		</div><!-- / .container -->
+	</section><!-- / #index -->
+
+
 <?php get_footer(); ?>

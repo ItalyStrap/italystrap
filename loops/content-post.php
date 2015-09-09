5b75c11fb07f1e891c9f7ae4fec14915e422add6
Enea Overclokk <info@overclokk.net>
2015-09-09 14:05:10 +0200

    New file for loop

---
+++ loops/content-post.php
@@ -0,0 +1,37 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header>
+		<h2>
+			<a itemprop="url" href="<?php the_permalink(); ?>" title="<?php the_title_attribute() ?>" rel="bookmark">
+				<span itemprop="name">
+					<?php the_title(); ?>
+				</span>
+			</a>
+		</h2>
+	</header>
+	<footer>
+		<?php get_template_part('template/meta'); ?>
+	</footer>
+	<meta  itemprop="image" content="<?php echo italystrap_thumb_url();?>"/>
+	<section>
+		<?php if ( has_post_thumbnail() ) { ?>
+			<div class="margin-bottom-25 thumbnail">
+				<figure>
+			  		<?php
+			  		the_post_thumbnail(
+			  			'article-thumb',
+			  			array(
+			  				'class' => 'center-block img-responsive',
+			  				'alt'   => trim( strip_tags( get_post_meta( get_post_thumbnail_id( $post->ID ), '_wp_attachment_image_alt', true ) ) ),
+			  				) );
+			  		?>
+				</figure>
+			</div>
+		<?php } ?>
+		<div  itemprop="text"><?php the_excerpt(); ?></div>
+		<p class="sr-only"><?php _e('Last edit:', 'ItalyStrap'); ?> <time datetime="<?php the_modified_time('Y-m-d') ?>" itemprop="dateModified"><?php the_modified_time('d F Y') ?></time></p>
+		<span class="clearfix"></span>
+	</section>
+		<?php echo italystrap_ttr_wc();?>
+		<span class="clearfix"></span>
+</article>
+<hr>

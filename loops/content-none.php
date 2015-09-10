4da9081e5fd04157bd25f5ef5f065651318eaf9b
Enea Overclokk <info@overclokk.net>
2015-09-10 16:50:08 +0200

    Added new fallback

--- loops/content-none.php
+++ loops/content-none.php
@@ -1,18 +1,39 @@
-<article class="no-results not-found">
+<section class="no-results not-found">
 	<header class="page-header">
-		<h1 itemprop="headline">
-			<?php _e( 'Damn, what you were looking for is not there', 'ItalyStrap' ); ?>
+		<h1  class="page-title" itemprop="headline">
+			<?php _e( 'What you were looking for is not there', 'ItalyStrap' ); ?>
 		</h1>
 	</header>
-		<p>
-			<?php _e( 'Search again with the form below.', 'ItalyStrap' ); ?>
-		</p>
-		<?php
+	<div class="page-content">
 
-		get_search_form();
+		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-		echo italystrap_get_404_image( 'margin-top-25 img-responsive center-block' );
-		
-		?>
-</article><!-- #post-0 -->
+			<p>
+				<?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'ItalyStrap' ), esc_url( admin_url( 'post-new.php' ) ) ); ?>
+			</p>
+
+		<?php elseif ( is_search() ) : ?>
+
+			<p>
+				<?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'ItalyStrap' ); ?>
+			</p>
+
+			<?php
+			get_search_form();
+			echo italystrap_get_404_image( 'margin-top-25 img-responsive center-block' );
+
+			else : ?>
+
+			<p>
+				<?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'ItalyStrap' ); ?>
+			</p>
+			<?php
+			get_search_form();
+			echo italystrap_get_404_image( 'margin-top-25 img-responsive center-block' );
+
+			endif; ?>
+
+	</div><!-- .page-content -->
+
+</section><!-- #post-0 -->
 <hr>

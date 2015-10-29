22f9c2827f012c63e137ee3b130c578da650cefd
Enea Overclokk <info@overclokk.net>
2015-10-29 09:22:42 +0100

    WPCS standard

--- lib/script.php
+++ lib/script.php
@@ -1,19 +1,25 @@
 <?php
 /**
  * Use this file for loading any scripts and styles
- * If you use child theme copy it in child folder and change $path if necessary
+ * If you use child theme copy it in child folder and change ITALYSTRAP_PARENT_PATH if necessary
+ *
+ * @package ItalyStrap
+ * @since 1.0.0
  */
-function italystrap_add_style_and_script(){
+
+/**
+ * Init script and style
+ */
+function italystrap_add_style_and_script() {
 
 	/**
-	 * @variable $path is path for parent template
-	 * @variable $pathchild is for child template
+	 * Avoid caching script
+	 * @var int
 	 */
-	global $path;
-	global $pathchild;
+	$ver = ( WP_DEBUG ) ? rand( 0, 100000 ) : null;
 
 	/**
-	 * Only for 
+	 * Only for
 	 * @link http://www.bootstrapcdn.com/alpha/
 	 */
 	// wp_register_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha/css/bootstrap.min.css', null, null, null );
@@ -22,74 +28,81 @@ function italystrap_add_style_and_script(){
 	/**
 	 * Load Bootstrap styles
 	 */
-	wp_enqueue_style( 'bootstrap',  $path . '/css/bootstrap.min.css', null, null, null);
+	wp_enqueue_style( 'bootstrap',  ITALYSTRAP_PARENT_PATH . '/css/bootstrap.min.css', null, $ver, null );
 
 	/**
 	 * Deregister jquery from WP
 	 */
-	wp_deregister_script('jquery');
+	wp_deregister_script( 'jquery' );
 
 	/**
 	 * Load jquery from google CDN
 	 */
-	wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', false, null, true);
-	
+	wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', false, $ver, true );
+
 	/**
 	 * If CDN is down load from callback
 	 */
-	add_filter('script_loader_src', 'italystrap_jquery_local_fallback', 10, 2);
-	wp_enqueue_script('jquery');
+	add_filter( 'script_loader_src', 'italystrap_jquery_local_fallback', 10, 2 );
+	wp_enqueue_script( 'jquery' );
 
 	/**
 	 * Load script JS and CSS with conditional tags
 	 */
-	if ( is_home() || is_front_page() ){
+	if ( is_home() || is_front_page() ) {
 
-		wp_enqueue_style( 'home',  $path . '/css/home.css', array('bootstrap'), null, null);
-		wp_enqueue_script( 'home', $path . '/js/home.min.js', array('jquery'), null,  true );
+		wp_enqueue_style( 'home',  ITALYSTRAP_PARENT_PATH . '/css/home.css', array( 'bootstrap' ), $ver, null );
+		wp_enqueue_script( 'home', ITALYSTRAP_PARENT_PATH . '/js/home.min.js', array( 'jquery' ), $ver,  true );
 
-	}elseif ( is_singular() ) {
+	} elseif ( is_singular() ) {
 
-		wp_enqueue_style( 'singular',  $path . '/css/singular.css', array('bootstrap'), null, null);
-		wp_enqueue_script( 'singular', $path . '/js/singular.min.js', array('jquery'), null,  true );
+		wp_enqueue_style( 'singular',  ITALYSTRAP_PARENT_PATH . '/css/singular.css', array( 'bootstrap' ), $ver, null );
+		wp_enqueue_script( 'singular', ITALYSTRAP_PARENT_PATH . '/js/singular.min.js', array( 'jquery' ), $ver,  true );
 
-	}elseif ( is_archive() ) {
+	} elseif ( is_archive() ) {
 
-		wp_enqueue_style( 'archive',  $path . '/css/archive.css', array('bootstrap'), null, null);
-		wp_enqueue_script( 'archive', $path . '/js/archive.min.js', array('jquery'), null,  true );
+		wp_enqueue_style( 'archive',  ITALYSTRAP_PARENT_PATH . '/css/archive.css', array( 'bootstrap' ), $ver, null );
+		wp_enqueue_script( 'archive', ITALYSTRAP_PARENT_PATH . '/js/archive.min.js', array( 'jquery' ), $ver,  true );
 
-	}else {
+	} else {
 
-		wp_enqueue_style( 'custom',  $path . '/css/custom.css', array('bootstrap'), null, null);
-		wp_enqueue_script( 'custom', $path . '/js/custom.min.js', array('jquery'), null,  true );
+		wp_enqueue_style( 'custom',  ITALYSTRAP_PARENT_PATH . '/css/custom.css', array( 'bootstrap' ), $ver, null );
+		wp_enqueue_script( 'custom', ITALYSTRAP_PARENT_PATH . '/js/custom.min.js', array( 'jquery' ), $ver,  true );
 
 	}
 
 	/**
 	 * Load comment-reply script
 	 */
-	if (is_singular() && comments_open() && get_option('thread_comments'))
-		wp_enqueue_script('comment-reply');
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
 }
 
 /**
- * http://wordpress.stackexchange.com/a/12450
- * https://github.com/roots/roots/blob/master/lib/scripts.php
+ * Print fallback if google CDN is out
+ *
+ * @link http://wordpress.stackexchange.com/a/12450
+ * @link https://github.com/roots/roots/blob/master/lib/scripts.php
+ *
+ * @since 1.0.0
+ *
+ * @param  string $src    jQuery src if true = $add_jquery_fallback.
+ * @param  string $handle Name of handle.
+ * @return string         Return jQuery fallback if true = $add_jquery_fallback
  */
-function italystrap_jquery_local_fallback($src, $handle = null){
+function italystrap_jquery_local_fallback( $src, $handle = null ) {
 
-	global $path;
-	global $pathchild;
 	static $add_jquery_fallback = false;
 
-	if ($add_jquery_fallback) {
-		echo '<script>window.jQuery || document.write(\'<script src="' . $path . '/js/jquery.min.js"><\/script>\')</script>' . "\n";
+	if ( $add_jquery_fallback ) {
+
+		echo '<script>window.jQuery || document.write(\'<script src="' . ITALYSTRAP_PARENT_PATH . '/js/jquery.min.js"><\/script>\')</script>' . "\n";
 		$add_jquery_fallback = false;
+
 	}
 
-	if ($handle === 'jquery') {
+	if ( 'jquery' === $handle )
 		$add_jquery_fallback = true;
-	}
 
 	return $src;
 }
@@ -97,11 +110,12 @@ function italystrap_jquery_local_fallback($src, $handle = null){
 /**
  * Hook into the 'wp_enqueue_scripts' action
  */
-add_action( 'wp_enqueue_scripts', 'italystrap_add_style_and_script' ); 
+add_action( 'wp_enqueue_scripts', 'italystrap_add_style_and_script' );
 add_action( 'wp_footer', 'italystrap_jquery_local_fallback' );
 
 /**
  * Add Custom CSS in visual editor
+ *
  * @link http://codex.wordpress.org/Function_Reference/add_editor_style
  *
  * Leggere qui perché forse c'è un problema con i font, non prende il path giusto
@@ -110,13 +124,10 @@ add_action( 'wp_footer', 'italystrap_jquery_local_fallback' );
  */
 function italystrap_add_editor_styles() {
 
-	global $path;
-	global $pathchild;
-
 	$arg = array(
-		// $path . '/css/visual_editor.css',
-		$path . '/css/bootstrap.min.css',
-		$path . '/css/admin.css'
+		// ITALYSTRAP_PARENT_PATH . '/css/visual_editor.css',
+		ITALYSTRAP_PARENT_PATH . '/css/bootstrap.min.css',
+		ITALYSTRAP_PARENT_PATH . '/css/admin.css',
 		);
 
 	add_editor_style( $arg );

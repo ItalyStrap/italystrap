9040a748a09f77edf79f6730f19751da69641689
overclokk <info@overclokk.net>
2015-11-03 09:04:29 +0100

    deprecated function

--- lib/comment_reply.php
+++ lib/comment_reply.php
@@ -19,6 +19,8 @@
  */
 function new_get_cancel_comment_reply_link( $text = '', $class = '' ) {
 
+	_deprecated_function( __FUNCTION__, 'ItalyStrap 3.1' );
+
 	if ( empty( $text ) )
 		$text = __('Click here to cancel reply.', 'ItalyStrap');
 

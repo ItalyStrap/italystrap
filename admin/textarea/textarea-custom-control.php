cd699fd4d7c14c8e64c0470da11c49c4dd47c693
overclokk <info@overclokk.net>
2015-11-03 09:06:53 +0100

    Changed row

--- admin/textarea/textarea-custom-control.php
+++ admin/textarea/textarea-custom-control.php
@@ -21,7 +21,7 @@ class Textarea_Custom_Control extends WP_Customize_Control
 		?>
 		<label>
 			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-			<textarea class="large-text" cols="20" rows="5" <?php $this->link(); ?>>
+			<textarea class="large-text" cols="20" rows="10" <?php $this->link(); ?>>
 				<?php echo esc_textarea( $this->value() ); ?>
 			</textarea>
 		</label>

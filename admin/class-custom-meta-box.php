01adf434dcc81f123551edf89295ec38b3bfc2df
overclokk <info@overclokk.net>
2016-02-13 11:32:25 +0100

    New test for layout selection

--- admin/class-custom-meta-box.php
+++ admin/class-custom-meta-box.php
@@ -1,24 +1,36 @@
 <?php namespace ItalyStrap\Admin;
 /**
-* Initialize custom meta box build with CMB2
-*
-*
-* @package ItalyStrap\Admin
-* @version 1.0
-* @since   4.0.0
-*/
+ * Initialize custom meta box build with CMB2
+ *
+ * @package ItalyStrap\Admin
+ * @version 1.0
+ * @since   4.0.0
+ */
 
 /**
  * Add some custom meta box in many areas of WordPress
  */
 class Custom_Meta_Box {
 
+	/**
+	 * CMB prefix
+	 *
+	 * @var string
+	 */
 	private $prefix = '';
-	
+
+	/**
+	 * Init the constructor
+	 */
 	function __construct() {
 
+		/**
+		 * Start with an underscore to hide fields from custom fields list
+		 *
+		 * @var string
+		 */
 		$this->prefix = '_italystrap_';
-		
+
 	}
 
 	/**
@@ -26,37 +38,38 @@ class Custom_Meta_Box {
 	 */
 	public function register_post_metabox() {
 
-		// Start with an underscore to hide fields from custom fields list
-		// $this->prefix = 'gjav_';
-		//
 		/**
 		 * Sample metabox to demonstrate each field type included
 		 */
-		$cmb = new_cmb2_box( array(
-			'id'            => $this->prefix . 'metabox',
-			'title'         => __( 'Advanced settings', 'ItalyStrap' ),
-			'object_types'  => array( 'product', 'post' ), // Post type
-			// 'show_on_cb' => 'yourprefix_show_if_front_page', // function should return a bool value
-			'context'    => 'side',
-			'priority'   => 'low',
-		) );
-
-		$cmb->add_field( array(
-			'name'		=> __( 'Test Multi Checkbox', 'ItalyStrap' ),
-			'desc'		=> __( 'Advance layout setting for this page/post', 'ItalyStrap' ),
-			'id'		=> 'layout_options',
-			'type'		=> 'multicheck',
-			'options'	=> array(
-				'title'			=> __( 'Hide title', 'ItalyStrap' ),
-				'meta'			=> __( 'Hide meta', 'ItalyStrap' ),
-				'thumb'			=> __( 'Hide feautured image', 'ItalyStrap' ),
-				'content'		=> __( 'Hide the content', 'ItalyStrap' ),
-				'author'		=> __( 'Hide author', 'ItalyStrap' ),
-				'social'		=> __( 'Hide builtin social sharing', 'ItalyStrap' ),
-				'comments'		=> __( 'Hide comments', 'ItalyStrap' ),
-				'comments_form'	=> __( 'Hide comments form', 'ItalyStrap' ),
+		$cmb = new_cmb2_box(
+			array(
+				'id'            => $this->prefix . 'metabox',
+				'title'         => __( 'Advanced settings', 'ItalyStrap' ),
+				'object_types'  => array( 'product', 'post' ),
+				'context'    => 'side',
+				'priority'   => 'low',
 			)
-		) );
+		);
+
+		$cmb->add_field(
+			array(
+				'name'		=> __( 'Layout options', 'ItalyStrap' ),
+				'desc'		=> __( 'Advance layout setting for this page/post', 'ItalyStrap' ),
+				'id'		=> 'layout_options',
+				'type'		=> 'multicheck',
+				'options'	=> array(
+					'title'			=> __( 'Hide title', 'ItalyStrap' ),
+					'meta'			=> __( 'Hide meta', 'ItalyStrap' ),
+					'thumb'			=> __( 'Hide feautured image', 'ItalyStrap' ),
+					'content'		=> __( 'Hide the content', 'ItalyStrap' ),
+					'author'		=> __( 'Hide author', 'ItalyStrap' ),
+					'social'		=> __( 'Hide builtin social sharing', 'ItalyStrap' ),
+					'comments'		=> __( 'Hide comments', 'ItalyStrap' ),
+					'comments_form'	=> __( 'Hide comments form', 'ItalyStrap' ),
+					'sidebar'		=> __( 'Hide sidebar', 'ItalyStrap' ),
+				),
+			)
+		);
 
 	}
 }

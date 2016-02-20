7bc6da69cf2a379e04eabff6afd2b47221863393
overclokk <info@overclokk.net>
2016-02-20 21:10:31 +0100

    New options layout file for future dev

---
+++ options/options-layout.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Array definition for Layout options
+ *
+ * @package ItalyStrap
+ * @since 4.0.0
+ */
+
+if ( ! defined( 'ITALYSTRAP_THEME' ) or ! ITALYSTRAP_THEME ) {
+	die();
+}
+
+return array(
+
+);

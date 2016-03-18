04b10a673e90afaa89b71e96e9bfde321357db52
overclokk <info@overclokk.net>
2016-03-18 12:57:57 +0100

    Fixed pager in case there is only 1 link

--- loops/content-single.php
+++ loops/content-single.php
@@ -97,15 +97,18 @@
  * @var array
  */
 $args = array(
-	'previous_format'	=> '<ul class="pager"><li class="previous pager-prev"> %link',
+	'previous_format'	=> '<li class="previous pager-prev"> %link',
 	'previous_link'		=> '<i class="glyphicon glyphicon-arrow-left"></i> %title</li>',
 	'next_format'		=> '<li class="next pager-next"> %link',
-	'next_link'			=> '%title <i class="glyphicon glyphicon-arrow-right"></i></li></ul>',
+	'next_link'			=> '%title <i class="glyphicon glyphicon-arrow-right"></i></li>',
 	);
 $args = apply_filters( 'italystrap_previous_next_post_link_args', $args );
-
-previous_post_link( $args['previous_format'], $args['previous_link'] );
-next_post_link( $args['next_format'], $args['next_link'] );
 ?>
+<ul class="pager">
+	<?php
+	previous_post_link( $args['previous_format'], $args['previous_link'] );
+	next_post_link( $args['next_format'], $args['next_link'] );
+	?>
+</ul>
 <span class="clearfix"></span>
 <hr>

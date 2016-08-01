878e9059b453417811e409eabdef52a51378f550
overclokk <info@overclokk.net>
2016-08-01 15:50:34 +0200

    Some code for asset API

--- lib/script.php
+++ lib/script.php
@@ -43,18 +43,18 @@ function italystrap_add_style_and_script() {
 	/**
 	 * Deregister jquery from WP
 	 */
-	wp_deregister_script( 'jquery' );
+	// wp_deregister_script( 'jquery' );
 
 	/**
 	 * Load jquery from google CDN
 	 */
-	wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', false, $ver, true );
+	// wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', false, $ver, true );
 
 	/**
 	 * If CDN is down load from callback
 	 */
 	add_filter( 'script_loader_src', 'italystrap_jquery_local_fallback', 10, 2 );
-	wp_enqueue_script( 'jquery' );
+	// wp_enqueue_script( 'jquery' );
 
 	/**
 	 * Load script JS and CSS with conditional tags
@@ -85,23 +85,31 @@ function italystrap_add_style_and_script() {
 	 * Load comment-reply script
 	 */
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
+		wp_enqueue_script( 'comment-reply', null, null, null, true );
 
 
 	$config_styles = array(
-		array(
-			'handle'	=> 'bootstrap',
-			// 'file'		=> STYLESHEETPATH . '/css/bootstrap.min.css',
-			'file'		=> ITALYSTRAP_PARENT_PATH . '/css/bootstrap.min.css',
-			'deps'		=> null,
-			'version'	=> $ver,
-			'media'		=> null,
-			// 'load_on'	=> 'is_single',
-		),
+		// array(
+		// 	'handle'	=> 'bootstrap',
+		// 	'file'		=> 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha/css/bootstrap.min.css',
+		// 	'deps'		=> null,
+		// 	'version'	=> $ver,
+		// 	'media'		=> null,
+		// 	'pre_register'	=> true,
+		// ),
+		// array(
+		// 	'handle'	=> 'bootstrap',
+		// 	// 'file'		=> STYLESHEETPATH . '/css/bootstrap.min.css',
+		// 	'file'		=> ITALYSTRAP_PARENT_PATH . '/css/bootstrap.min.css',
+		// 	'deps'		=> null,
+		// 	'version'	=> $ver,
+		// 	'media'		=> null,
+		// 	// 'load_on'	=> 'is_single',
+		// ),
 		array(
 			'handle'	=> CURRENT_TEMPLATE_SLUG,
 			'file'		=> file_exists( STYLESHEETPATH . '/css/' . CURRENT_TEMPLATE_SLUG . '.css' ) ? ITALYSTRAP_PARENT_PATH . '/css/' . CURRENT_TEMPLATE_SLUG . '.css' : ITALYSTRAP_PARENT_PATH . '/css/custom.css',
-			'deps'		=> array( 'bootstrap' ),
+			// 'deps'		=> array( 'bootstrap' ),
 			'version'	=> $ver,
 			'media'		=> null,
 			// 'load_on'	=> 'is_single',
@@ -110,6 +118,29 @@ function italystrap_add_style_and_script() {
 
 	$style = new ItalyStrap\Core\Style( $config_styles );
 	$style->register();
+
+	$config_scripts = array(
+		array(
+			'handle'		=> 'jquery',
+			'file'			=> 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
+			'deps'			=> false,
+			'version'		=> $ver,
+			'in_footer'		=> true,
+			'pre_register'	=> true,
+			'deregister'	=> true, // This will deregister previous registered script.
+		),
+		array(
+			'handle'		=> CURRENT_TEMPLATE_SLUG,
+			'file'			=> file_exists( STYLESHEETPATH . '/js/' . CURRENT_TEMPLATE_SLUG . $min . '.js' ) ? ITALYSTRAP_PARENT_PATH . '/js/' . CURRENT_TEMPLATE_SLUG . $min . '.js' : ITALYSTRAP_PARENT_PATH . '/js/custom' . $min . '.js',
+			'deps'			=> array( 'jquery' ),
+			'version'		=> $ver,
+			'in_footer'		=> true,
+			// 'load_on'	=> 'is_single',
+		),
+	);
+
+	$script = new ItalyStrap\Core\Script( $config_scripts );
+	$script->register();
 
 	// echo "<pre>";
 	// var_dump( $style );
@@ -151,26 +182,3 @@ function italystrap_jquery_local_fallback( $src, $handle = null ) {
  */
 add_action( 'wp_enqueue_scripts', 'italystrap_add_style_and_script' );
 add_action( 'wp_footer', 'italystrap_jquery_local_fallback' );
-
-/**
- * Add Custom CSS in visual editor
- *
- * @link http://codex.wordpress.org/Function_Reference/add_editor_style
- *
- * Leggere qui perché forse c'è un problema con i font, non prende il path giusto
- * @link http://codeboxr.com/blogs/adding-twitter-bootstrap-support-in-wordpress-visual-editor
- * @link https://www.google.it/search?q=wordpress+add+css+bootstrap+visual+editor&oq=wordpress+add+css+bootstrap+visual+editor&gs_l=serp.3...893578.895997.0.896668.10.10.0.0.0.3.388.1849.0j1j4j2.7.0....0...1c.1.52.serp..8.2.732.wb3nJL89Fxk
- */
-function italystrap_add_editor_styles() {
-
-	$arg = array(
-		// ITALYSTRAP_PARENT_PATH . '/css/visual_editor.css',
-		ITALYSTRAP_PARENT_PATH . '/css/bootstrap.min.css',
-		ITALYSTRAP_PARENT_PATH . '/css/admin.css',
-		);
-
-	add_editor_style( $arg );
-
-}
-
-add_action( 'after_setup_theme', 'italystrap_add_editor_styles' );

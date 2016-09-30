07115efa0c0f0c5c77c43872d501d737b649faaf
overclokk <info@overclokk.net>
2016-09-30 20:48:30 +0200

    New functionality for child themes

--- functions.php
+++ functions.php
@@ -42,3 +42,53 @@ function italystrap( $file_name = 'content' ) {
 	require locate_template( '/template/' . $file_name . '.php' );
 
 }
+
+/**
+ * Fires once ItalyStrap theme has loaded.
+ *
+ * @since 2.0.0
+ */
+do_action( 'italystrap_theme_loaded' );
+
+/**
+ * This filter is used to load your php file right after ItalyStrap theme is loaded.
+ * The purpose is to have al code in the same scope without using global
+ * with variables provided from this theme.
+ *
+ * Usage example:
+ *
+ * 1 - First of all you have to have the file/files with some code
+ *     that extending this themes functionality in your theme path.
+ * 2 - Than you have to activate your theme.
+ * 3 - And then see the below example.
+ *
+ * add_filter( 'italystrap_require_theme_files_path', 'add_your_files_path' );
+ *
+ * function add_your_files_path( array $arg ) {
+ *     return array_merge(
+ *                  $arg,
+ *                  array( STYLESHEETPATH . 'my-dir/my-file.php' )
+ *     );
+ * }
+ * Important:
+ * Remeber that the file you want to load just after ItalyStrap theme
+ * has not to be required/included from your theme because
+ * you will get an error 'You can't redeclare...'.
+ *
+ * @since 2.0.0
+ *
+ * @var array
+ */
+$theme_files_path = apply_filters( 'italystrap_require_theme_files_path', array() );
+
+if ( ! empty( $theme_files_path ) ) {
+	foreach ( (array) $theme_files_path as $key => $theme_file_path ) {
+		require( $theme_file_path );
+	}
+	/**
+	 * Fires once ItalyStrap Child theme has loaded.
+	 *
+	 * @since 2.0.0
+	 */
+	do_action( 'italystrap_child_theme_loaded' );
+}

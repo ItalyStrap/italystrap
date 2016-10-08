237784cfcc91d957925182e58d8c7471e466b275
overclokk <info@overclokk.net>
2016-10-08 13:51:31 +0200

    Better formatting

--- front-page.php
+++ front-page.php
@@ -1,5 +1,22 @@
 <?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ItalyStrap
+ * @since 1.0.0
+ */
 
 namespace ItalyStrap;
+
+if ( ! defined( 'ABSPATH' ) or ! ABSPATH ) {
+	die();
+}
 
 italystrap();

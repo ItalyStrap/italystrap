24f253f5beb7b3d98d481a1413a34546d8032c0b
overclokk <info@overclokk.net>
2016-12-19 21:14:33 +0100

    Refactoring of the script.php

--- core/Asset/examples.php
+++ core/Asset/examples.php
@@ -38,14 +38,28 @@ function add_style_and_script() {
 	}
 
 	$config_styles = array(
+		/**
+		 * Only for
+		 * @link http://www.bootstrapcdn.com/alpha/
+		 */
+		// array(
+		// 	'handle'	=> 'bootstrap',
+		// 	'file'		=> 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha/css/bootstrap.min.css',
+		// 	'deps'		=> null,
+		// 	'version'	=> $ver,
+		// 	'media'		=> null,
+		// 	'pre_register'	=> true,
+		// ),
 		array(
 			'handle'	=> CURRENT_TEMPLATE_SLUG,
 			'file'		=>
 				file_exists( STYLESHEETPATH . '/css/' . $dev_dir . CURRENT_TEMPLATE_SLUG . '.css' )
 				? STYLESHEETURL . '/css/' . $dev_dir . CURRENT_TEMPLATE_SLUG . '.css'
 				: STYLESHEETURL . '/css/' . $dev_dir . 'custom.css',
+			// 'deps'		=> array( 'bootstrap' ),
 			'version'	=> $ver,
 			'media'		=> null,
+			// 'load_on'	=> 'is_single',
 		),
 	);
 
@@ -64,10 +78,7 @@ function add_style_and_script() {
 		),
 		array(
 			'handle'		=> CURRENT_TEMPLATE_SLUG,
-			'file'			=>
-				file_exists( STYLESHEETPATH . '/js/' . CURRENT_TEMPLATE_SLUG . $min . '.js' )
-				? STYLESHEETURL . '/js/' . CURRENT_TEMPLATE_SLUG . $min . '.js'
-				: STYLESHEETURL . '/js/custom' . $min . '.js',
+			'file'			=> file_exists( STYLESHEETPATH . '/js/' . CURRENT_TEMPLATE_SLUG . $min . '.js' ) ? STYLESHEETURL . '/js/' . CURRENT_TEMPLATE_SLUG . $min . '.js' : STYLESHEETURL . '/js/custom' . $min . '.js',
 			'deps'			=> array( 'jquery' ),
 			'version'		=> $ver,
 			'in_footer'		=> true,
@@ -80,48 +91,9 @@ function add_style_and_script() {
 
 	$script = new Script( $config_scripts );
 	$script->register();
-
-	/**
-	 * If CDN is down load from callback
-	 */
-	add_filter( 'script_loader_src', __NAMESPACE__ . '\jquery_local_fallback', 10, 2 );
-}
-
-/**
- * Print fallback if google CDN is out
- *
- * @link http://wordpress.stackexchange.com/a/12450
- * @link https://github.com/roots/roots/blob/master/lib/scripts.php
- *
- * @since 1.0.0
- *
- * @param  string $src    jQuery src if true = $add_jquery_fallback.
- * @param  string $handle Name of handle.
- * @return string         Return jQuery fallback if true = $add_jquery_fallback
- */
-function jquery_local_fallback( $src, $handle = null ) {
-
-	static $add_jquery_fallback = false;
-
-	if ( $add_jquery_fallback ) {
-
-		/**
-		 * @todo document.write è da sostituire.
-		 */
-		echo '<script>window.jQuery || document.write(\'<script src="' . TEMPLATEURL . '/js/jquery.min.js"><\/script>\')</script>' . "\n";
-		$add_jquery_fallback = false;
-
-	}
-
-	if ( 'jquery' === $handle ) {
-		$add_jquery_fallback = true;
-	}
-
-	return $src;
 }
 
 /**
  * Hook into the 'wp_enqueue_scripts' action
  */
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\add_style_and_script' );
-add_action( 'wp_footer', __NAMESPACE__ . '\jquery_local_fallback' );

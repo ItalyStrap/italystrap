9a030bf536ff7ec2958242735d8925fe2855fd80
overclokk <info@overclokk.net>
2017-01-09 19:04:19 +0100

    Updated examples

--- core/Asset/examples.php
+++ core/Asset/examples.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * This file handle loading scripts
+ * Here you can find some example of how to add script and style from child theme.
+ * Some OOP example soon.
  *
  * @package ItalyStrap
  * @since 1.0.0
@@ -8,13 +9,7 @@
 
 namespace ItalyStrap\Core;
 
-use ItalyStrap\Core\Asset\Style as Style;
-use ItalyStrap\Core\Asset\Script as Script;
-
-/**
- * Init script and style
- */
-function add_style_and_script() {
+add_filter( 'italystrap_config_enqueue_style', function ( array $style ) {
 
 	$min = '.min';
 
@@ -37,63 +32,76 @@ function add_style_and_script() {
 
 	}
 
-	$config_styles = array(
+	/**
+	 * Only for
+	 * @link http://www.bootstrapcdn.com/alpha/
+	 */
+	$style[] = array(
+		'handle'		=> 'bootstrap',
+		'file'			=> 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha/css/bootstrap.min.css',
+		'deps'			=> null,
+		'version'		=> $ver,
+		'media'			=> null,
 		/**
-		 * Only for
-		 * @link http://www.bootstrapcdn.com/alpha/
+		 * With external file you have to preregister the style
 		 */
-		// array(
-		// 	'handle'	=> 'bootstrap',
-		// 	'file'		=> 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha/css/bootstrap.min.css',
-		// 	'deps'		=> null,
-		// 	'version'	=> $ver,
-		// 	'media'		=> null,
-		// 	'pre_register'	=> true,
-		// ),
-		array(
-			'handle'	=> CURRENT_TEMPLATE_SLUG,
-			'file'		=>
-				file_exists( STYLESHEETPATH . '/css/' . $dev_dir . CURRENT_TEMPLATE_SLUG . '.css' )
-				? STYLESHEETURL . '/css/' . $dev_dir . CURRENT_TEMPLATE_SLUG . '.css'
-				: STYLESHEETURL . '/css/' . $dev_dir . 'custom.css',
-			// 'deps'		=> array( 'bootstrap' ),
-			'version'	=> $ver,
-			'media'		=> null,
-			// 'load_on'	=> 'is_single',
-		),
+		'pre_register'	=> true,
+		/**
+		 * If you want to load only with WP_DEBUG true.
+		 */
+		'load_on'		=> WP_DEBUG, // bool
 	);
 
-	$style = new Style( $config_styles );
-	$style->register();
-
-	$config_scripts = array(
-		array(
-			'handle'		=> 'jquery',
-			'file'			=> 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
-			'deps'			=> false,
-			'version'		=> $ver,
-			'in_footer'		=> true,
-			'pre_register'	=> true,
-			'deregister'	=> true, // This will deregister previous registered jQuery.
-		),
-		array(
-			'handle'		=> CURRENT_TEMPLATE_SLUG,
-			'file'			=> file_exists( STYLESHEETPATH . '/js/' . CURRENT_TEMPLATE_SLUG . $min . '.js' ) ? STYLESHEETURL . '/js/' . CURRENT_TEMPLATE_SLUG . $min . '.js' : STYLESHEETURL . '/js/custom' . $min . '.js',
-			'deps'			=> array( 'jquery' ),
-			'version'		=> $ver,
-			'in_footer'		=> true,
-		),
-		array(
-			'handle'		=> 'comment-reply',
-			'load_on'		=> 'ItalyStrap\Core\is_comment_reply',
-		),
+	$style[] = array(
+		'handle'	=> CURRENT_TEMPLATE_SLUG,
+		'file'		=>
+			file_exists( STYLESHEETPATH . '/css/' . $dev_dir . CURRENT_TEMPLATE_SLUG . '.css' )
+			? STYLESHEETURL . '/css/' . $dev_dir . CURRENT_TEMPLATE_SLUG . '.css'
+			: STYLESHEETURL . '/css/' . $dev_dir . 'custom.css',
+		// 'deps'		=> array( 'bootstrap' ),
+		'version'	=> $ver,
+		'media'		=> null,
+		// 'load_on'	=> 'is_single',
 	);
 
-	$script = new Script( $config_scripts );
-	$script->register();
-}
+	return $style;
 
-/**
- * Hook into the 'wp_enqueue_scripts' action
- */
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\add_style_and_script' );
+}, 10, 1 );
+
+add_filter( 'italystrap_config_enqueue_script', function ( array $script ) {
+
+	$script[] = array(
+		'handle'	=> 'livereload', // Required
+		'file'		=> '//localhost:35729/livereload.js', // Required
+		'in_footer'		=> true,
+		'load_on'		=> WP_DEBUG,
+	);
+
+	$script[] = array(
+		'handle'		=> 'jquery',
+		'file'			=> 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
+		'deps'			=> false,
+		'version'		=> $ver,
+		'in_footer'		=> true,
+		'deregister'	=> true, // This will deregister previous registered jQuery.
+		'pre_register'	=> true, // And this will register the new jQuery.
+	);
+
+	$script[] = array(
+		'handle'		=> CURRENT_TEMPLATE_SLUG,
+		'file'			=> file_exists( STYLESHEETPATH . '/js/' . CURRENT_TEMPLATE_SLUG . $min . '.js' ) ? STYLESHEETURL . '/js/' . CURRENT_TEMPLATE_SLUG . $min . '.js' : STYLESHEETURL . '/js/custom' . $min . '.js',
+		'deps'			=> array( 'jquery' ),
+		'version'		=> $ver,
+		'in_footer'		=> true,
+	);
+
+	$script[] = array(
+		/**
+		 * You can call a preregistered script.
+		 */
+		'handle'		=> 'comment-reply',
+		'load_on'		=> 'ItalyStrap\Core\is_comment_reply',
+	);
+
+	return $script;
+}, 10, 1 );

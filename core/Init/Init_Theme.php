7d00214b1cd64684604cca416e921ee8b7afcf27
overclokk <info@overclokk.net>
2017-03-13 01:01:28 +0100

    Added test for starter content

--- core/Init/Init_Theme.php
+++ core/Init/Init_Theme.php
@@ -74,7 +74,7 @@ class Init_Theme{
 		 * @see ItalyStrap\Core\Image\Size() for set_post_thumbnail_size()
 		 *
 		 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-		*/
+		 */
 		add_theme_support( 'post-thumbnails' );
 
 		/**
@@ -167,6 +167,127 @@ class Init_Theme{
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
 		/**
+		 * Add support to WooCommerce
+		 *
+		 * @since 4.0.0
+		 */
+		add_theme_support( 'woocommerce' );
+
+		/**
+		 * Define and register starter content to showcase the theme on new sites.
+		 * @see twentyseventeen
+		 * @link https://make.wordpress.org/core/2016/11/30/starter-content-for-themes-in-4-7/
+		 *
+		 * @var array
+		 */
+		$starter_content = array(
+			'widgets' => array(
+				// Place three core-defined widgets in the sidebar area.
+				'sidebar-1' => array(
+					'text_business_info',
+					'search',
+					'text_about',
+				),
+
+				// Add the core-defined business info widget to the footer 1 area.
+				'sidebar-2' => array(
+					'text_business_info',
+				),
+
+				// Put two core-defined widgets in the footer 2 area.
+				'sidebar-3' => array(
+					'text_about',
+					'search',
+				),
+			),
+
+			// Specify the core-defined pages to create and add custom thumbnails to some of them.
+			'posts' => array(
+				'home',
+				'about' => array(
+					'thumbnail' => '{{image-sandwich}}',
+				),
+				'contact' => array(
+					'thumbnail' => '{{image-espresso}}',
+				),
+				'blog' => array(
+					'thumbnail' => '{{image-coffee}}',
+				),
+				'homepage-section' => array(
+					'thumbnail' => '{{image-espresso}}',
+				),
+			),
+
+			// Create the custom image attachments used as post thumbnails for pages.
+			'attachments' => array(
+				'image-espresso' => array(
+					'post_title' => _x( 'Espresso', 'Theme starter content', 'twentyseventeen' ),
+					'file' => 'assets/images/espresso.jpg', // URL relative to the template directory.
+				),
+				'image-sandwich' => array(
+					'post_title' => _x( 'Sandwich', 'Theme starter content', 'twentyseventeen' ),
+					'file' => 'assets/images/sandwich.jpg',
+				),
+				'image-coffee' => array(
+					'post_title' => _x( 'Coffee', 'Theme starter content', 'twentyseventeen' ),
+					'file' => 'assets/images/coffee.jpg',
+				),
+			),
+
+			// Default to a static front page and assign the front and posts pages.
+			'options' => array(
+				'show_on_front' => 'page',
+				'page_on_front' => '{{home}}',
+				'page_for_posts' => '{{blog}}',
+			),
+
+			// Set the front page section theme mods to the IDs of the core-registered pages.
+			'theme_mods' => array(
+				'panel_1' => '{{homepage-section}}',
+				'panel_2' => '{{about}}',
+				'panel_3' => '{{blog}}',
+				'panel_4' => '{{contact}}',
+			),
+
+			// Set up nav menus for each of the two areas registered in the theme.
+			'nav_menus' => array(
+				// Assign a menu to the "top" location.
+				'main-menu' => array(
+					'name' => __( 'Top Menu', 'twentyseventeen' ),
+					'items' => array(
+						'link_home', // Note that the core "home" page is actually a link in case a static front page is not used.
+						'page_about',
+						'page_blog',
+						'page_contact',
+					),
+				),
+
+				// Assign a menu to the "social" location.
+				'social-menu' => array(
+					'name' => __( 'Social Links Menu', 'twentyseventeen' ),
+					'items' => array(
+						'link_yelp',
+						'link_facebook',
+						'link_twitter',
+						'link_instagram',
+						'link_email',
+					),
+				),
+			),
+		);
+
+		/**
+		 * Filters Twenty Seventeen array of starter content.
+		 *
+		 * @since Twenty Seventeen 1.1
+		 *
+		 * @param array $starter_content Array of starter content.
+		 */
+		$starter_content = apply_filters( 'italystrap_starter_content', $starter_content );
+
+		add_theme_support( 'starter-content', $starter_content );
+
+		/**
 		 * This theme uses wp_nav_menu() in one location.
 		 */
 		$nav_menus_locations = array(
@@ -177,13 +298,6 @@ class Init_Theme{
 			'footer-menu'		=> __( 'Footer Menu', 'ItalyStrap' ),
 			);
 		register_nav_menus( apply_filters( 'register_nav_menu_locations', $nav_menus_locations ) );
-
-		/**
-		 * Add support to WooCommerce
-		 *
-		 * @since 4.0.0
-		 */
-		add_theme_support( 'woocommerce' );
 
 	}
 

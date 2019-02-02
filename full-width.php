fb1c6b39da25b7e1f7962bf81820bca71131b09c
overclokk <info@overclokk.net>
2019-02-02 16:40:49 +0100

    Added example for changing thumb size

--- full-width.php
+++ full-width.php
@@ -20,4 +20,22 @@ if ( ! defined( 'ABSPATH' ) or ! ABSPATH ) {
 	die();
 }
 
+/**
+ * Change the thumbnail size for the old template full-width.php
+ *
+ * @param  string $size    The thumbnail size.
+ * @param  string $context The context.
+ * @return string          The thumbnail size.
+ */
+function post_thumbnail_size( $size, $context ) {
+
+	if ( is_page_template( 'full-width.php' ) ) {
+		return 'full-width';
+	}
+
+	return $size;
+
+}
+add_action( 'italystrap_post_thumbnail_size', __NAMESPACE__ . '\post_thumbnail_size', 10, 2 );
+
 italystrap();

6db9a905a2271fb7ab7b5ad86c3f828b7e3a7d16
overclokk <info@overclokk.net>
2019-02-08 12:46:33 +0100

    Added function for storing all config files

--- functions/config-helpers.php
+++ functions/config-helpers.php
@@ -9,3 +9,41 @@ namespace ItalyStrap\Config;
 function merge_array_to_config( Config_Interface $config, array $array_to_merge = [] ) {
 	$config->merge( $array_to_merge );
 }
+
+/**
+ * Retrieve an array of config files.
+ *
+ *
+ * @since 4.0.0
+ * @access private
+ *
+ * @see \wp_get_mu_plugins() file wp-includes/load.php
+ *
+ * @return array Files to include.
+ */
+function get_config_files() {
+
+	$config_files = [];
+	$config_dir = PARENTPATH . '/config';
+
+	if ( ! is_dir( $config_dir ) ) {
+		return $config_files;
+	}
+
+	if ( ! $dh = opendir( $config_dir ) ) {
+		return $config_files;
+	}
+
+	while ( ( $file = readdir( $dh ) ) !== false ) {
+		if ( substr( $file, -4 ) === '.php' && 'index.php' !== $file ) {
+			$key = str_replace( '.php', '', $file );
+			$config_files[ $key ] = $config_dir . '/' . $file;
+		}
+	}
+
+	closedir( $dh );
+
+//	sort( $config_files );
+
+	return (array) $config_files;
+}

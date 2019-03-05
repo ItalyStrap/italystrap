f9770126189b4cbd626cd4c0bb3b9f4f02abab2d
overclokk <info@overclokk.net>
2019-03-05 15:47:46 +0100

    New schema config in alpha

---
+++ config/schema.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Still in beta
+ */
+
+return [
+	/**
+	 * Filter name
+	 */
+	'italystrap_content_attr'	=> [
+		'itemscope' => true,
+		'itemtype' => 'https://schema.org/CollectionPage',
+	],
+
+];

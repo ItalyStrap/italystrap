06501803b39ea34a000dfb336e4a1b1dff744732
overclokk <info@overclokk.net>
2019-03-15 20:39:51 +0100

    Try to refacotor the loader

--- functions/italystrap.php
+++ functions/italystrap.php
@@ -11,15 +11,35 @@ namespace ItalyStrap;
 /**
  * Function for loading the template.
  *
- * @param  string $file_name The file_name on this function is called.
+ * @param string $file
+ * @param array $data
+ * @throws \Auryn\ConfigException
+ * @throws \Auryn\InjectionException
  */
-function italystrap( $file_name = 'index' ) {
+function italystrap( $file = 'index', array $data = [] ) {
+
+	$injector = \ItalyStrap\Factory\get_injector();
+
+	$view = $injector->share('\ItalyStrap\Template\View')->make('\ItalyStrap\Template\View');
+
+	$base_structure = [
+//		'italystrap_loop'	=> 'posts/loop',
+		'italystrap'		=> 'index',
+	];
 
 	$template_dir = apply_filters( 'italystrap_template_dir', 'templates' );
 
-	require locate_template(
-		$template_dir . DIRECTORY_SEPARATOR . $file_name . '.php'
-	);
+	foreach ( $base_structure as $filter => $file ) {
+
+		$template = $template_dir . DIRECTORY_SEPARATOR . $file;
+		add_filter( $filter, function() use ( $view, $template, $data ) {
+			echo $view->render( $template, $data );
+		} );
+	}
+
+//		$template = $template_dir . DIRECTORY_SEPARATOR . $file;
+//
+//		echo $view->render( $template, $data );
 
 	do_action( 'italystrap' );
 }

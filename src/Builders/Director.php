063ca8f6fbeb91a35a9173027d4a476f2fbadaed
overclokk <info@overclokk.net>
2020-02-15 20:43:19 +0100

    Director

--- src/Builders/Director.php
+++ src/Builders/Director.php
@@ -10,42 +10,23 @@ declare(strict_types=1);
 
 namespace ItalyStrap\Builders;
 
+use ItalyStrap\Event\EventManager;
 use ItalyStrap\Event\SubscriberInterface;
 use function \ItalyStrap\Factory\get_event_manager;
 
 class Director implements SubscriberInterface {
+	/**
+	 * @var EventManager
+	 */
+	private $eventManager;
 
 	/**
-	 * Returns an array of events (hooks) that this subscriber wants to register with
-	 * the Events Manager API.
-	 *
-	 * The array key is the name of the hook. The value can be:
-	 *
-	 *  * The method name
-	 *  * An array with the method name and priority
-	 *  * An array with the method name, priority and number of accepted arguments
-	 *
-	 * For instance:
-	 *
-	 * array(
-	 *     // 'event_name'             => 'method_name',
-	 *     'italystrap_before_header'  => 'render',
-	 * )
-	 * array(
-	 *     // 'event_name'                     => 'method_name',
-	 *     'italystrap_before_entry_content'   => array(
-	 *         'function_to_add'   => 'render',
-	 *         'priority'          => 30, // Default 10
-	 *         'accepted_args'     => 1 // Default 1
-	 *     ),
-	 * );
-	 *
 	 * @return array
 	 */
 	public function getSubscribedEvents(): array {
 		return [
-//			'wp'	=> 'create_page', // @TODO is it a good hook? Or I have to create one just before the 'italystrap'
-			'italystrap_build'	=> 'create_page', // @TODO is it a good hook? Or I have to create one just before the 'italystrap'
+//			'wp'	=> 'createPage', // @TODO is it a good hook? Or I have to create one just before the 'italystrap'
+			'italystrap_build'	=> 'createPage', // @TODO is it a good hook? Or I have to create one just before the 'italystrap'
 //			'italystrap_theme_loaded'	=> 'apply', // @TODO is it a good hook? Or I have to create one just before the 'italystrap'
 		];
 	}
@@ -64,16 +45,18 @@ class Director implements SubscriberInterface {
 	 * Director constructor.
 	 *
 	 * @param Builder_Interface $builder
+	 * @param EventManager $eventManager
 	 */
-	public function __construct( Builder_Interface $builder ) {
+	public function __construct( Builder_Interface $builder, EventManager $eventManager ) {
 		$this->builder = $builder;
 //		$this->parse_Attr = $parse_Attr;
+		$this->eventManager = $eventManager;
 	}
 
 	/**
 	 * Create the page
 	 */
-	public function create_page() {
+	public function createPage() {
 
 		/**
 		 * Injector setter is run on dependencies.php
@@ -96,6 +79,6 @@ class Director implements SubscriberInterface {
 	 * Remove from hooks
 	 */
 	public function destroy() {
-		get_event_manager()->remove_subscriber( $this );
+		$this->eventManager->removeSubscriber( $this );
 	}
 }

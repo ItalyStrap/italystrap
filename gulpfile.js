d55f56ec4d968a0cc8cf0230a516562a9be84b39
overclokk <info@overclokk.net>
2020-03-05 08:03:09 +0100

    Gulp css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,43 @@
 const gulp = require('gulp');
 const path = require('path');
 const compass = require('gulp-compass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const pixrem = require('pixrem');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('compass', function() {
-    return gulp.src('.assets/sass/*.scss')
+    return gulp.src('./assets/sass/*.scss')
         .pipe(compass({
-            project: path.join(__dirname),
-            import_path: 'bower/bootstrap-sass/assets/stylesheets',
-            css: 'css',
+            project: path.join(__dirname, 'assets'),
+            import_path: __dirname + '/bower/bootstrap-sass/assets/stylesheets',
+            css: 'css/src',
             sass: 'sass'
-        }))
+        }));
+        // .pipe(gulp.dest('css/src'));
+});
+
+gulp.task('css', function () {
+    const browser = {
+        overrideBrowserslist: ['last 2 version']
+    };
+
+    const plugins = [
+        autoprefixer(browser),
+        pixrem(browser),
+        cssnano({
+            preset: ['default', {
+                discardComments: {
+                    removeAll: true,
+                },
+            }]
+        })
+    ];
+
+    return gulp.src('./assets/css/src/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss(plugins))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./assets/css'));
 });

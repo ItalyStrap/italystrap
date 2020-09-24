a9f427b219055d9a790ba60128c9958900e5732f
Enea <info@overclokk.net>
2020-09-24 20:19:16 +0200

    Removed part of the old comment

--- functions.php
+++ functions.php
@@ -1,24 +1,6 @@
 <?php
 /**
- * ItalyStrap functions and definitions
- *
- * Set up the theme and provides some helper functions, which are used in the
- * theme as custom template tags. Others are attached to action and filter
- * hooks in WordPress to change core functionality.
- *
- * When using a child theme you can override certain functions (those wrapped
- * in a function_exists() call) by defining them first in your child theme's
- * functions.php file. The child theme's functions.php file is included before
- * the parent theme's file, so the child theme functions would be used.
- *
- * @link https://codex.wordpress.org/Theme_Development
- * @link https://codex.wordpress.org/Child_Themes
- *
- * Functions that are not pluggable (not wrapped in function_exists()) are
- * instead attached to a filter or action hook.
- *
- * For more information on hooks, actions, and filters,
- * @link https://codex.wordpress.org/Plugin_API
+ * ItalyStrap the WordPress theme framework!
  *
  * @package ItalyStrap
  * @since 1.0.0

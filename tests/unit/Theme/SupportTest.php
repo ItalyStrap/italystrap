a8e0b6db39940c5231210a427493bcbff0a19031
Enea <info@overclokk.net>
2023-02-07 17:11:13 +0100

    CS

--- tests/unit/Theme/SupportTest.php
+++ tests/unit/Theme/SupportTest.php
@@ -5,32 +5,29 @@ namespace ItalyStrap\Tests\Theme;
 
 use ItalyStrap\Tests\BaseUnitTrait;
 
-class SupportTest extends \Codeception\Test\Unit
-{
+class SupportTest extends \Codeception\Test\Unit {
+
 
 	use BaseUnitTrait;
 
-    /**
-     * @var \UnitTester
-     */
-    protected $tester;
-    
-    protected function _before()
-    {
-    }
+	/**
+	 * @var \UnitTester
+	 */
+	protected $tester;
+	
+	protected function _before() {
+	}
 
-    protected function _after()
-    {
-    }
+	protected function _after() {
+	}
 
 	private function getInstance(): \ItalyStrap\Theme\Support {
 		$sut = new \ItalyStrap\Theme\Support();
 		return $sut;
 	}
 
-    // tests
-    public function instanceOk()
-    {
+	// tests
+	public function instanceOk() {
 		$this->getInstance();
-    }
+	}
 }

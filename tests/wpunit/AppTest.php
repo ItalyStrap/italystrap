abfcb6251cbc0dc52c3d494640eceb1b602d0ddd
Enea <info@overclokk.net>
2023-07-11 19:49:50 +0200

    Start some files polish

---
+++ tests/wpunit/AppTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class AppTest extends \Codeception\TestCase\WPTestCase
+{
+    /**
+     * @var \WpunitTester
+     */
+    protected $tester;
+    
+    public function setUp(): void
+    {
+        // Before...
+        parent::setUp();
+
+        // Your set up methods here.
+    }
+
+    public function tearDown(): void
+    {
+        // Your tear down methods here.
+
+        // Then...
+        parent::tearDown();
+    }
+
+    // Tests
+    public function test_it_works()
+    {
+        $post = static::factory()->post->create_and_get();
+        
+        $this->assertInstanceOf(\WP_Post::class, $post);
+    }
+}
